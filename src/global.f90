32ab41998bd4399978eab9769c50d666c6af1296
Congyue Cui <ccui@princeton.edu>
2020-05-25 00:53:05 -0400

    update

--- src/global.f90
+++ src/global.f90
@@ -104,7 +104,7 @@ module global
       endif
 
       if (myrank .eq. 0) then
-        print *, iloop, nall, ntotal, nall / ntotal
+        print *, iloop, nall, ntotal, nall / ntotal, perc
       endif
 
     enddo
