9a15d3ddade5200401769304ebe4da1d4b15afda
Congyue Cui <ccui@princeton.edu>
2020-10-28 01:15:58 -0400

    update

--- src/precond_kernels.f90
+++ src/precond_kernels.f90
@@ -90,7 +90,7 @@ program precond_kernels
 
   implicit none
 
-  integer, parameter :: NKERNELS = 6    !bulk_betah, bulk_betav, bulk_c, eta
+  integer, parameter :: NKERNELS = 5    !bulk_betah, bulk_betav, bulk_c, eta
   character(len=500), parameter :: kernel_names(NKERNELS) = &
     (/character(len=500) :: "bulk_betah_kl_crust_mantle", &
                             "bulk_betav_kl_crust_mantle", "bulk_c_kl_crust_mantle", &
