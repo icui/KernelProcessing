7b0f0c53b26f04109a4335a53ef7abd386a05c86
Congyue Cui <ccui@princeton.edu>
2020-10-28 01:49:15 -0400

    update

--- src/precond_kernels.f90
+++ src/precond_kernels.f90
@@ -90,7 +90,8 @@ program precond_kernels
 
   implicit none
 
-  character(len=500), parameter :: kernel_names(5) = &
+  integer, parameter :: NKERNELS = 5    !bulk_betah, bulk_betav, bulk_c, eta
+  character(len=500), parameter :: kernel_names(NKERNELS) = &
     (/character(len=500) :: "bulk_betah_kl_crust_mantle", &
                             "bulk_betav_kl_crust_mantle", "bulk_c_kl_crust_mantle", &
                             "eta_kl_crust_mantle", "rho_kl_crust_mantle"/)
@@ -98,8 +99,8 @@ program precond_kernels
     (/character(len=500) :: "hess_kl_crust_mantle"/)
 
   real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC):: hess = 0.0, invHess = 0.0
-  real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC, 5):: kernels = 0.0
-  real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC, 6):: kernels_precond = 0.0
+  real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC, NKERNELS):: kernels = 0.0
+  real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC, NKERNELS+1):: kernels_precond = 0.0
   real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC, 1):: kernels2 = 0.0
 
   character(len=500) :: input_file, input_file2, output_file
@@ -120,8 +121,8 @@ program precond_kernels
   call prepare_hessian(hess, threshold_hess, invHess)
 
   ! precond the kernel
-  do iker = 1, 6
-    if(iker == 6) then
+  do iker = 1, NKERNELS+1
+    if(iker == NKERNELS+1) then
       ! assign the invHess back
       kernels_precond(:, :, :, :, iker) = invHess
     else
