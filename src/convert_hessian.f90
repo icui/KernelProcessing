08897604ec50ec3c08db0d85d1675420d0c421b5
Congyue Cui <ccui@princeton.edu>
2021-03-19 16:15:50 -0400

    update

--- src/convert_hessian.f90
+++ src/convert_hessian.f90
@@ -8,7 +8,7 @@
 ! gradients of other parameters that the preconditioner to be applied. I currently smooth the preconditioner and
 ! the other gradients in the same way.
 
-module inverse_hessian_sub
+module convert_hessian_sub
   use mpi
   use global, only : max_all_all_cr, min_all_all_cr, CUSTOM_REAL, exit_mpi, &
                      myrank, quantile_all_all_cr
@@ -16,100 +16,58 @@ module inverse_hessian_sub
 
   contains
 
-  subroutine get_sys_args(input_hess, input_model, output_hess, threshold_hess)
+  subroutine get_sys_args(input_hess, input_model, output_hess)
     character(len=*), intent(inout) :: input_hess, input_model, output_hess
-    real(kind=CUSTOM_REAL), intent(inout) :: threshold_hess
-
-    character(len=20) :: threshold_str
 
     call getarg(1, input_hess)
     call getarg(2, input_model)
     call getarg(3, output_hess)
-    call getarg(4, threshold_str)
-
-    read(threshold_str, *) threshold_hess
 
     if(input_hess == '' .or. input_model == '' .or. output_hess == '') then
-      call exit_mpi("Usage: xinverse_hessian input_hess input_model output_hess threshold_hess")
+      call exit_mpi("Usage: xconvert_hessian input_hess input_model output_hess")
     endif
 
     if(myrank == 0) then
       write(*, *) "Input hessian: ", trim(input_hess)
       write(*, *) "Input model: ", trim(input_model)
       write(*, *) "Output hessian: ", trim(output_hess)
-      write(*, *) "Threshold hessian: ", threshold_hess
     endif
 
   end subroutine get_sys_args
 
-  subroutine prepare_hessian(hess, threshold, hess_inv)
-    real(CUSTOM_REAL), dimension(:, :, :, :), intent(inout) :: hess, hess_inv
-    real(CUSTOM_REAL), intent(in) :: threshold
+end module convert_hessian_sub
 
-    real(kind=CUSTOM_REAL):: maxh_all, minh_all, damp
-
-    hess = abs(hess)
-    call max_all_all_cr(maxval(hess), maxh_all)
-    call min_all_all_cr(minval(hess), minh_all)
-
-    if ( maxh_all < 1.e-18 ) then
-      call exit_mpi("hess max value < 1.e-18")
-    endif
-
-    if (myrank==0) then
-      write(*, *) "Max and Min of hess: ", maxh_all, minh_all
-    endif
-
-    ! normalized hess
-    damp = maxh_all / (threshold - 1)
-    hess = (hess + damp) / (maxh_all + damp)
-    
-    call max_all_all_cr(maxval(hess), maxh_all)
-    call min_all_all_cr(minval(hess), minh_all)
-
-    if (myrank==0) then
-      write(*, *) 'min and max hess after norm', minh_all, maxh_all
-      write(*, *) "Hessian condition number: ", threshold
-    endif
-
-    hess_inv = 1.0_CUSTOM_REAL / hess
-
-  end subroutine prepare_hessian
-
-end module inverse_hessian_sub
-
-program inverse_hessian
+program convert_hessian
   use mpi
   use adios_read_mod
   use AdiosIO
   use global, only : NGLLX, NGLLY, NGLLZ, NSPEC, myrank, CUSTOM_REAL
   use global, only : init_mpi
-  use inverse_hessian_sub
+  use convert_hessian_sub
 
   implicit none
 
   character(len=500), parameter :: hess_names(3) = &
     (/character(len=500) :: "hess_kappa_kl_crust_mantle", "hess_mu_kl_crust_mantle", "hess_rho_kl_crust_mantle"/)
 
-  character(len=500), parameter :: invhess_names(4) = &
-    (/character(len=500) :: "invhess_vs_kl_crust_mantle", "invhess_vp_kl_crust_mantle", "invhess_eta_kl_crust_mantle", &
-                            "invhess_rho_kl_crust_mantle"/)
+  character(len=500), parameter :: hess2_names(4) = &
+    (/character(len=500) :: "hess_vs_kl_crust_mantle", "hess_vp_kl_crust_mantle", "hess_eta_kl_crust_mantle", &
+                            "hess_rho_kl_crust_mantle"/)
 
   character(len=500), parameter :: model_names(3) = &
     (/character(len=500) :: "reg1/kappavstore", "reg1/muvstore", "reg1/muhstore"/)
 
   real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC, 3):: hess = 0.0, models = 0.0
-  real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC, 4):: invhess
+  real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC, 4):: hess2
   real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC):: kappa, mu
   real(kind=CUSTOM_REAL),dimension(NGLLX, NGLLY, NGLLZ, NSPEC):: hess_kappa, hess_mu, hess_rho, hess_vp, hess_vs, hess_eta
 
   character(len=500) :: input_hess, input_model, output_hess
-  real(kind=CUSTOM_REAL) :: threshold_hess
   integer:: ier
 
   call init_mpi()
 
-  call get_sys_args(input_hess, input_model, output_hess, threshold_hess)
+  call get_sys_args(input_hess, input_model, output_hess)
   call adios_read_init_method(ADIOS_READ_METHOD_BP, MPI_COMM_WORLD, "verbose=1", ier)
 
   call read_bp_file_real(input_hess, hess_names, hess)
@@ -126,21 +84,14 @@ program inverse_hessian
   hess_vs = 4.0 * hess_mu + 64.0 / 9.0 * (mu/kappa)**2 * hess_kappa
   hess_eta = hess_kappa + 4.0 / 9.0 * hess_mu
 
-  ! vs
-  call prepare_hessian(hess_vs, threshold_hess, invhess(:, :, :, :, 1))
+  hess2(:, :, :, :, 1) = abs(hess_vs)
+  hess2(:, :, :, :, 2) = abs(hess_vp)
+  hess2(:, :, :, :, 3) = abs(hess_eta)
+  hess2(:, :, :, :, 4) = abs(hess_rho)
 
-  ! vp
-  call prepare_hessian(hess_vp, threshold_hess, invhess(:, :, :, :, 2))
-
-  ! eta
-  call prepare_hessian(hess_eta, threshold_hess, invhess(:, :, :, :, 3))
-
-  ! rho
-  call prepare_hessian(hess_rho, threshold_hess, invhess(:, :, :, :, 4))
-
-  call write_bp_file(invhess, invhess_names, "KERNEL_GOURPS", output_hess)
+  call write_bp_file(hess2, hess2_names, "KERNEL_GOURPS", output_hess)
 
   call adios_finalize(myrank, ier)
   call MPI_FINALIZE(ier)
 
-end program inverse_hessian
+end program convert_hessian
